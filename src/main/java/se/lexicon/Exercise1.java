66daa6bff323b5560f03e15d3a2cbfc146452ea8
Mikael Aurell <aurell.mikael@gmail.com>
2020-12-20 17:00:25 +0100

    added game4 (exercise4)

--- src/main/java/se/lexicon/Exercise1.java
+++ src/main/java/se/lexicon/Exercise1.java
@@ -1,5 +1,6 @@
 package se.lexicon;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
@@ -34,9 +35,9 @@ public class Exercise1 {
                 case "3":
                     sortStringArray();
                     break;
-                /*case "4":
+                case "4":
                     copyStringArray();
-                    break;*/
+                    break;
                 case "0":
                     break;
 
@@ -165,6 +166,30 @@ public class Exercise1 {
 
             for (String string_ : capitals) {
              System.out.println(string_);
+            }
+            finish = yesOrNo();
+        }
+    }
+
+    public static void copyStringArray() {
+
+        boolean finish = false;
+
+        while (!finish) {
+
+            String[] countries = new String[arraySize()];
+
+            IntStream.range(0, countries.length).forEach(i -> {
+                int downInt = countries.length - i;
+                System.out.print("You have: " + downInt + " more Countries to enter. \n Please enter the Countries of array index [" + i + "]: ");
+                Scanner capitalScanner = new Scanner(System.in);
+                countries[i] = capitalScanner.nextLine();
+            });
+
+            String[] copyCountries = (Arrays.copyOf(countries, countries.length));
+
+            for (String string_ : copyCountries) {
+                System.out.println(string_);
             }
             finish = yesOrNo();
         }
